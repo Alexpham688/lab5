61de3dfb5c1f0c082f3337d3ab15cf14f3a30e64
Alexpham688 <Alexpham688@gmail.com>
2017-04-13 13:43:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var x =70;
+
+var y = (x - 32)  / 1.8;
+
+console.log(y);
+console.log( 'farhenheit temp' + 'converted temp' = 'celcius temp');
+
+var y = 21.1
+
+var x = ( y * 1.8) + 32;
+
+console.log(x);
