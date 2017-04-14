77ba84078d0dd7c67cac0d52e582d1f74000f58b
Alexpham688 <Alexpham688@gmail.com>
2017-04-13 21:21:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var x =70;
+var farhenheit =70;
 
-var y = (x - 32)  / 1.8;
+var celcius = (farhenheit - 32)  / 1.8;
 
 console.log(y);
 console.log( 'farhenheit temp' + 'converted temp' = 'celcius temp');
@@ -9,4 +9,5 @@ var y = 21.1
 
 var x = ( y * 1.8) + 32;
 
-console.log(x);
+console.log(farhenheit);
+console.log( 'celcius temp' * 1.8 + 32);
